73bb4d6e3df52d9f462932cb5b97da51d6724232
Ilya Yukhnovskiy <ilya.yukhnovskiy@nedra.digital>
2022-07-12 00:34:01 +0300

    first commit

---
+++ microbenchmark.R
@@ -0,0 +1,59 @@
+#  install.packages(c("microbenchmark"))
+library(purrr)
+library(microbenchmark)
+library(tidyr)
+library(magrittr)
+library(dplyr)
+#--------------------------------------
+fib <- function(n){
+  stopifnot(n > 0)
+  if(n == 1){
+    0
+  } else if(n == 2){
+    1
+  } else {
+    fib(n - 1) + fib(n - 2)
+  }
+}
+#--------------------------------------
+fib_tbl <- c(0, 1, rep(NA, 23))
+
+fib_mem <- function(n){
+  stopifnot(n > 0)
+  
+  if(!is.na(fib_tbl[n])){
+    fib_tbl[n]
+  } else {
+    fib_tbl[n - 1] <<- fib_mem(n - 1)
+    fib_tbl[n - 2] <<- fib_mem(n - 2)
+    fib_tbl[n - 1] + fib_tbl[n - 2]
+  }
+}
+# ------------------------------------
+fib_data <- map(1:10, function(x){microbenchmark(fib(x), times = 100)$time})
+names(fib_data) <- paste0(letters[1:10], 1:10)
+fib_data <- as.data.frame(fib_data)
+
+fib_data %<>%
+  gather(num, time) %>%
+  group_by(num) %>%
+  summarise(med_time = median(time))
+
+memo_data <- map(1:10, function(x){microbenchmark(fib_mem(x))$time})
+names(memo_data) <- paste0(letters[1:10], 1:10)
+memo_data <- as.data.frame(memo_data)
+
+memo_data %<>%
+  gather(num, time) %>%
+  group_by(num) %>%
+  summarise(med_time = median(time))
+
+plot(1:10, fib_data$med_time, xlab = "Fibonacci Number", ylab = "Median Time (Nanoseconds)",
+     pch = 18, bty = "n", xaxt = "n", yaxt = "n")
+axis(1, at = 1:10)
+axis(2, at = seq(0, 350000, by = 50000))
+points(1:10 + .1, memo_data$med_time, col = "blue", pch = 18)
+legend(1, 300000, c("Not Memorized", "Memoized"), pch = 18, 
+       col = c("black", "blue"), bty = "n", cex = 1, y.intersp = 1.5)
+
+
